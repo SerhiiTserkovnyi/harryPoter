4111e2c38452d7a42334f60d214052d0b4b68788
Serhii Tserkovnyi <dizz413@gmail.com>
2022-03-06 22:38:50 +0200

    pagination page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 let boxContainer = document.querySelector('.box-container');
-async function getResponse() {
+async function getResponse(f, e) {
     let response = await (await fetch('http://hp-api.herokuapp.com/api/characters')).json();
 
     console.log(response);
-    response.slice(0, 25).forEach(({ name, alternate_names, species, image }) => {
+    response.slice(f, e).forEach(({ name, alternate_names, species, image }) => {
 
         boxContainer.innerHTML += `<div class="box"><div class="actor-name">${name}</div>
         <div class="species">${species}</div>
         <div class="image-film"><img src="${image}"></div></div>`;
     });
 
+
+
 }
-getResponse();
+
+
+getResponse(0, 5);
