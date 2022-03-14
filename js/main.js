f95f267ad1a92fbdd83c9375cc3725bb38ac7ad5
Serhii Tserkovnyi <dizz413@gmail.com>
2022-03-14 22:08:58 +0200

    review fetch

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 let boxContainer = document.querySelector('.box-container');
+let searchInput = document.querySelector('.searchInput');
 async function getResponse(f, e) {
-    let response = await (await fetch('http://hp-api.herokuapp.com/api/characters')).json();
-
-    console.log(response);
-    response.slice(f, e).forEach(({ name, alternate_names, species, image }) => {
-
+    response = await fetch('http://hp-api.herokuapp.com/api/characters')
+        .then(res => res.json());
+    response.slice(f, e).forEach(({ name, species, image }) => {
         boxContainer.innerHTML += `<div class="box"><div class="actor-name">${name}</div>
         <div class="species">${species}</div>
         <div class="image-film"><img src="${image}"></div></div>`;
     });
 
-
-
 }
+
 
 
 getResponse(0, 5);
